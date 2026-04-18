db0abc55feab3b716a2785cfa65bf4093b994d46
xxy <xxy@xxy5.com>
2026-04-18 14:45:35 +0800

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import '@/App.css'
+
+function App() {
+    return (
+        <main className="container">
+            <h1 className="text-3xl font-bold text-center text-gray-800">Welcome to Excel Cleaning App</h1>
+        </main>
+    )
+}
+
+export default App
